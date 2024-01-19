370354de0fe793e1c74353d66889edecba6c7ee9
LeeSM0518 <nalsm98@naver.com>
2024-01-19 23:44:28 +0900

    fix: env 인터페이스 추가

---
+++ oauth-frontend/env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    VITE_NAVER_LOGIN_API_HOST: string,
+    VITE_NAVER_LOGIN_URL: string,
+    VITE_NAVER_TOKEN_URL: string,
+    VITE_NAVER_CLIENT_ID: string,
+    VITE_NAVER_CLIENT_SECRET: string,
+    VITE_NAVER_REDIRECT_URL: string,
+    VITE_NAVER_STATE: string
+}
