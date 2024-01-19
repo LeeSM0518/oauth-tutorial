8bc20514ad1e14a23ef4da9516b0be925c0749dc
LeeSM0518 <nalsm98@naver.com>
2024-01-19 23:43:44 +0900

    fix: 불필요한 페이지들 제거

--- oauth-frontend/src/router/index.ts
+++ oauth-frontend/src/router/index.ts
@@ -1,23 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: import("@/components/Home.vue")
+        },
+        {
+            path: '/naver/login',
+            name: 'naver',
+            component: import("@/components/NaverLogin.vue")
+        }
+    ]
 })
 
 export default router
