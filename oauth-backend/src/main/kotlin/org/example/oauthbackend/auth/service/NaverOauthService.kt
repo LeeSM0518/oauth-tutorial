94240f436988af48637905cf7181cbcf2b4bbdac
LeeSM0518 <nalsm98@naver.com>
2024-01-21 17:44:17 +0900

    refactor: 사용되지 않는 import 제거 및 코드 리팩토링

--- oauth-backend/src/main/kotlin/org/example/oauthbackend/auth/service/NaverOauthService.kt
+++ oauth-backend/src/main/kotlin/org/example/oauthbackend/auth/service/NaverOauthService.kt
@@ -7,7 +7,6 @@ import org.example.oauthbackend.auth.config.NaverOauthProperties
 import org.example.oauthbackend.auth.controller.dto.OauthLoginRequest
 import org.example.oauthbackend.auth.exception.NAVER_OAUTH_LOGIN_FAIL_EXCEPTION
 import org.example.oauthbackend.auth.exception.OauthLoginFailException
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -19,20 +18,22 @@ class NaverOauthService(
 
     fun getEmail(request: OauthLoginRequest): String = getEmail(getToken(request))
 
-    private fun getEmail(token: String) = (naverProfileClient
-        .getEmail(token)
-        .email
-        ?: throw OauthLoginFailException(NAVER_OAUTH_LOGIN_FAIL_EXCEPTION))
+    private fun getEmail(token: String): String =
+        naverProfileClient
+            .getEmail(token)
+            .email
+            ?: throw OauthLoginFailException(NAVER_OAUTH_LOGIN_FAIL_EXCEPTION)
 
-    private fun getToken(request: OauthLoginRequest) = (naverOauthClient
-        .getToken(
-            grantType = NaverGrantType.AUTHORIZATION_CODE.value,
-            clientId = naverOauthProperties.clientId,
-            clientSecret = naverOauthProperties.clientSecret,
-            code = request.authorizationCode,
-            state = naverOauthProperties.state
-        )
-        .takeIf { it.token != null }
-        ?.token
-        ?: throw OauthLoginFailException(NAVER_OAUTH_LOGIN_FAIL_EXCEPTION))
+    private fun getToken(request: OauthLoginRequest): String =
+        naverOauthClient
+            .getToken(
+                grantType = NaverGrantType.AUTHORIZATION_CODE.value,
+                clientId = naverOauthProperties.clientId,
+                clientSecret = naverOauthProperties.clientSecret,
+                code = request.authorizationCode,
+                state = naverOauthProperties.state
+            )
+            .takeIf { it.token != null }
+            ?.token
+            ?: throw OauthLoginFailException(NAVER_OAUTH_LOGIN_FAIL_EXCEPTION)
 }
