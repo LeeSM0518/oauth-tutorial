0794df8f3c8fb432a0c61ea8fa5f0d0ae1447bd9
LeeSM0518 <nalsm98@naver.com>
2024-01-21 21:00:16 +0900

    feat: .env 예시 파일 업로드

--- oauth-frontend/env.d.ts
+++ oauth-frontend/env.d.ts
@@ -4,11 +4,7 @@ interface ImportMetaEnv {
     VITE_BASE_URL: string,
     VITE_BACKEND_AUTH_URL: string,
     VITE_NAVER_LOGIN_API_HOST: string,
-    VITE_NAVER_LOGIN_URL: string,
-    VITE_NAVER_TOKEN_URL: string,
-    VITE_NAVER_USER_URL: string,
     VITE_NAVER_CLIENT_ID: string,
-    VITE_NAVER_CLIENT_SECRET: string,
     VITE_NAVER_REDIRECT_URL: string,
     VITE_NAVER_STATE: string
 }
